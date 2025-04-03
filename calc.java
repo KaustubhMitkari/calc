356f98a4582d887e6f25ad507587165f0a9e194c
KaustubhMitkari <80772697+KaustubhMitkari@users.noreply.github.com>
2025-04-03 15:26:01 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public class AddTwoNumbers {
+public class AddT {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first number: ");
+        System.out.print("Enter  number: ");
         double num1 = scanner.nextDouble();
-        System.out.print("Enter second number: ");
+        System.out.print("Enter number: ");
         double num2 = scanner.nextDouble();
         double sum = num1 + num2;
         System.out.println("The sum is: " + sum);
